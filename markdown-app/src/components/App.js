d3d9c4127fa80ae014feea9a0114a9292d08c0f1
jv88899 <jv88899@gmail.com>
2018-12-25 08:56:10 -0600

    feat(markdown-app) add eidtor component

--- markdown-app/src/components/App.js
+++ markdown-app/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Editor from './editor/Editor';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="app">
+                <Editor />
             </div>
         )
     }
